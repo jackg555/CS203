994bf170a3e704374b1418079eebfc350647392a
jackgiddens <jackgnz555@gmail.com>
2023-06-13 17:13:29 +1200

    User can now add data to Lists table and view it

--- main.py
+++ main.py
@@ -1,72 +1,47 @@
-from flask import Flask,request, render_template
+from flask import Flask, request, render_template, jsonify
 import sqlite3
 
 app = Flask (__name__)
-
-#@app.route('/groceries', methods=['GET','POST'])
-#def insertFood():
-    #conn = sqlite3.connect('groceries.sqlite')
-    #cursor = conn.cursor()
-
-    #if request.method == 'GET':
-        #sql_search = """SELECT * FROM groceries"""
-        #cursor.execute(sql_search)
-        #food = cursor.fetchall()
-        #return {"food": food}, 200
-
-    #elif request.method == 'POST':
-        #request_data = request.get_json()
-        #new_food = {
-            #"name": request_data['name'],
-            #"cdprice": request_data['cdprice'],
-            #"nwprice": request_data['nwprice'],
-            #"pnsprice": request_data['pnsprice']
-        #}
-
-        #sql_query = """INSERT INTO groceries (name, cdprice, nwprice, pnsprice) VALUES (?, ?, ?, ?)"""
-        #cursor.execute(sql_query, (new_food['name'], new_food['cdprice'], new_food['nwprice'], new_food['pnsprice']))
-        #conn.commit()
-        #return new_food, 201
-
 @app.route('/', methods=['GET'])
 def main():
-    return render_template('addfood.html')
-
-@app.route('/viewfood', methods=['GET'])
-def viewFood():
-    conn = sqlite3.connect('groceries.sqlite')
+    conn = sqlite3.connect('shoppingDB.sqlite')
     cursor = conn.cursor()
 
-    sql_fetch_query = """SELECT * FROM groceries"""
+    sql_fetch_query = """SELECT * FROM Lists"""
     cursor.execute(sql_fetch_query)
-    groceries = cursor.fetchall()
+    Lists = cursor.fetchall()
 
-    return render_template('viewfood.html', food_content = groceries)
+    return render_template('addlist.html', list_content = Lists)
 
-@app.route('/addfood', methods=['POST'])
-def addFood():
-    conn = sqlite3.connect('groceries.sqlite')
+@app.route('/viewlistjson', methods=['GET'])
+def viewFoodJson():
+    conn = sqlite3.connect('shoppingDB.sqlite')
+    cursor = conn.cursor()
+
+    sql_fetch_query = """SELECT * FROM Lists"""
+    cursor.execute(sql_fetch_query)
+    Lists = cursor.fetchall()
+
+    return jsonify(Lists)
+
+@app.route('/addlist', methods=['POST'])
+def addList():
+    conn = sqlite3.connect('shoppingDB.sqlite')
     cursor = conn.cursor()
 
     received_data_obj = request.form
     data_obj_to_save = dict(received_data_obj)
 
     data_model = {
-        'name': data_obj_to_save['name'],
-        'cdprice': data_obj_to_save['cdprice'],
-        'nwprice': data_obj_to_save['nwprice'],
-        'pnsprice': data_obj_to_save['pnsprice']
+        'lname': data_obj_to_save['lname'],
+        'ldate': data_obj_to_save['ldate']
     }
 
-    sql_query = """INSERT INTO groceries (name, cdprice, nwprice, pnsprice) VALUES (?, ?, ?, ?)"""
-    cursor.execute(sql_query,
-                   (data_model['name'],
-                   data_model['cdprice'],
-                   data_model['nwprice'],
-                   data_model['pnsprice']))
+    sql_query = """INSERT INTO Lists (lname, ldate) VALUES (?, ?)"""
+    cursor.execute(sql_query, (data_model['lname'], data_model['ldate']))
     conn.commit()
 
-    return 'Food added successfully', 201
+    return 'List Added Successfully', 201
 
 if __name__ == '__main__':
     app.run(debug=True)
