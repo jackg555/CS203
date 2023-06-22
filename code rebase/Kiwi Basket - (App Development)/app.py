9fbb50e8c5f72f7c9ba47a139ba8e30d94f7e9a1
jackgiddens <jackgnz555@gmail.com>
2023-06-22 15:05:31 +1200

    program now displays the best supermarket to shop at

--- code rebase/Kiwi Basket - (App Development)/app.py
+++ code rebase/Kiwi Basket - (App Development)/app.py
@@ -3,24 +3,24 @@ import os
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
-
 db_path = os.path.join(app.root_path, 'instance', 'shoppingDB.sqlite')
 
 def fetch_list_items(list_id):
-    supermarket = 1
+    supermarket = get_best_supermarket(list_id)
 
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
-    sql_fetch_query = """SELECT iname, iquantity, price, ipricetype, lquantity, li.iid
-        FROM ListsItems li, Items i, SupermarketsItems si
+    sql_fetch_query = """SELECT iname, iquantity, price, ipricetype, lquantity, li.iid, s.sname
+        FROM ListsItems li, Items i, SupermarketsItems si, Supermarkets s
         WHERE li.lid=? 
         AND li.iid=i.iid 
         AND li.iid=si.iid
         AND si.sid=?
+        AND s.sid=?
         """
 
-    cursor.execute(sql_fetch_query, (list_id, supermarket,))
+    cursor.execute(sql_fetch_query, (list_id, supermarket, supermarket,))
     lists_items = cursor.fetchall()
 
     sql_fetch_query = """SELECT * FROM Lists WHERE lid=?"""
@@ -37,6 +37,49 @@ def fetch_list_items(list_id):
     conn.close()
 
     return lists_items, list_name, total_price
+
+def get_best_supermarket(list_id):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    test_query = """SELECT sid, price
+                        FROM ListsItems li, Items i, SupermarketsItems si
+                        WHERE li.lid=? 
+                        AND li.iid=i.iid 
+                        AND li.iid=si.iid
+                        """
+
+    cursor.execute(test_query, (list_id,))
+    supermarket_data = cursor.fetchall()
+
+    if len(supermarket_data) == 0:
+        return 1
+    else:
+        supermarket_dict = {}
+
+        for item in supermarket_data:
+            sid = item[0]
+            price = int(item[1])
+
+            if sid in supermarket_dict:
+                supermarket_dict[sid] += price
+            else:
+                supermarket_dict[sid] = price
+
+        values = list(supermarket_dict.values())
+        lowest_total = values[0]
+        supermarket = 1
+
+        for key in supermarket_dict:
+            item_total = supermarket_dict[key]
+
+            if (item_total < lowest_total):
+                lowest_total = item_total
+                supermarket = key
+
+        conn.close()
+
+        return str(supermarket)
 @app.route('/', methods=['GET'])
 def main():
     conn = sqlite3.connect(db_path)
@@ -62,7 +105,7 @@ def add_list():
     }
 
     sql_query = """INSERT INTO lists (lname, ldate) VALUES (?, ?)"""
-    cursor.execute(sql_query, (data_model['lname'], data_model['ldate']),)
+    cursor.execute(sql_query, (data_model['lname'], data_model['ldate'],))
     conn.commit()
     conn.close()
 
@@ -98,7 +141,7 @@ def disp_list(list_id):
         food_id = request.form.get('iid')
 
         check_query = """SELECT COUNT(*) FROM ListsItems WHERE lid = ? AND iid = ?"""
-        cursor.execute(check_query, (list_id, food_id),)
+        cursor.execute(check_query, (list_id, food_id,))
         count = cursor.fetchone()[0]
 
         pricetype_query = """SELECT ipricetype FROM Items WHERE iid = ?"""
@@ -112,10 +155,10 @@ def disp_list(list_id):
 
         if count == 0:
             sql_query = """INSERT INTO ListsItems (lid, iid, lquantity) VALUES (?, ?, ?)"""
-            cursor.execute(sql_query, (list_id, food_id, default_quantity),)
+            cursor.execute(sql_query, (list_id, food_id, default_quantity,))
         else:
             update_query = """UPDATE ListsItems SET lquantity = lquantity + ? WHERE lid = ? AND iid = ?"""
-            cursor.execute(update_query, (default_quantity, list_id, food_id),)
+            cursor.execute(update_query, (default_quantity, list_id, food_id,))
 
         conn.commit()
         conn.close()
@@ -137,17 +180,17 @@ def update_quantity(list_id):
     update_quantity = float(request.form.get('update'))
 
     sql_fetch_query = """SELECT lquantity FROM ListsItems WHERE iid=? AND lid=?"""
-    cursor.execute(sql_fetch_query, (item_id, list_id),)
+    cursor.execute(sql_fetch_query, (item_id, list_id,))
     quantity = cursor.fetchone()[0]
 
     updated_quantity = quantity + update_quantity
 
     if updated_quantity <= 0.001:
         delete_query = """DELETE FROM ListsItems WHERE iid=? AND lid=?"""
-        cursor.execute(delete_query, (item_id, list_id),)
+        cursor.execute(delete_query, (item_id, list_id,))
     else:
         update_query = """UPDATE ListsItems SET lquantity = ? WHERE iid=? AND lid=?"""
-        cursor.execute(update_query, (updated_quantity, item_id, list_id),)
+        cursor.execute(update_query, (updated_quantity, item_id, list_id,))
 
     conn.commit()
     conn.close()
